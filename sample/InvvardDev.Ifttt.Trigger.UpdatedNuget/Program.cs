c94f534730b0ed579fcccce4f9266ff637425a3b
Invvard <7305493+Invvard@users.noreply.github.com>
2023-12-29 22:52:00 -0500

    Add sample project

---
+++ sample/InvvardDev.Ifttt.Trigger.UpdatedNuget/Program.cs
@@ -0,0 +1,32 @@
+using InvvardDev.Ifttt.Service.Api.Trigger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder.Services);
+
+var webApp = builder.Build();
+
+Configure(app, builder.Environment);
+
+app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    
+    services.AddTriggers();
+}
+
+void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+{
+    if (environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+}
