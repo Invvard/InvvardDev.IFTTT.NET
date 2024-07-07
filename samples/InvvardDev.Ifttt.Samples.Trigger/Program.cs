72586bde21096d39490c616c03e287819a6e0616
Pierre Cavaroc <7305493+Invvard@users.noreply.github.com>
2024-07-07 13:21:30 -0400

    Reference App Insights

--- samples/InvvardDev.Ifttt.Samples.Trigger/Program.cs
+++ samples/InvvardDev.Ifttt.Samples.Trigger/Program.cs
@@ -9,7 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var clientIftttOptions = builder.Configuration.GetSection(ClientIftttOptions.DefaultSectionName).Get<ClientIftttOptions>();
 
-builder.Services.AddSwaggerGen(options => options.AddIftttServiceKeyScheme());
+builder.Services
+       .AddHttpLogging(logging =>
+       {
+           logging.RequestHeaders.Add("IFTTT-Test-Mode");
+           logging.RequestHeaders.Add("x-datadog-trace-id");
+           logging.RequestHeaders.Add("x-datadog-tags");
+           logging.RequestHeaders.Add("traceparent");
+       })
+       .AddApplicationInsightsTelemetry()
+       .AddSwaggerGen(options => options.AddIftttServiceKeyScheme());
 
 builder.Services
        .AddIftttToolkit(clientIftttOptions.ServiceKey)
@@ -20,6 +29,8 @@ builder.Services
 var app = builder.Build();
 
 app.UseRouting();
+
+app.UseHttpLogging();
 
 app.ConfigureIftttToolkit()
    .UseServiceKeyAuthentication()
